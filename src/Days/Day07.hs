85bad9d95975efe63ceffeafa59abc4e35aba0f5
limelier <limelier@pm.me>
2025-12-11 17:43:12 +0200

    07-2

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -2,33 +2,49 @@
 
 module Days.Day07 where
 
-import Data.Set (Set, difference, intersection, union)
-import qualified Data.Set as Set (fromList, map)
+import GHC.Arr
 
 solve :: String -> (Int, Int)
 solve input = do
-  let ics = map (map fst . indexedSymbols) $ lines input
-  let start : rest = ics
-  let (finalBeams, totalSplits) = applySplittersRec (Set.fromList start) rest
-  (totalSplits, 0)
+  let (startLine : splitterLines) = lines input
+  let beams = listArray (0, length startLine - 1) [if c == 'S' then (1 :: Int) else 0 | c <- startLine]
+  let (finalBeams, splits) = splitRec beams splitterLines
+  (splits, sum finalBeams)
 
-indexedSymbols :: String -> [(Int, Char)]
-indexedSymbols s = filter (\(_, c) -> c /= '.') $ indexed s
+-- recursively split the beam front with each line of splitters from the input
+-- return the final beam front and total number of splits
+splitRec :: Array Int Int -> [String] -> (Array Int Int, Int)
+splitRec beams [] = (beams, 0)
+splitRec beams (line:rest) = do
+  let splitters = splittersOf line
+  let (beams', splits) = splitMany beams splitters
+  let (finalBeams, remainingSplits) = splitRec beams' rest
+  (finalBeams, splits + remainingSplits)
 
+-- split the beam front with the given list of splitters
+-- return the new beam front and the number of splits
+splitMany :: Array Int Int -> [Int] -> (Array Int Int, Int)
+splitMany beams splitters = do
+  let splitters' = filter (\s -> beams!s /= 0) splitters
+  let beams' = foldl split beams splitters
+  (beams', length splitters')
+
+-- split the beam at pos into two copies, adding them to the previous and next beams
+split :: Array Int Int -> Int -> Array Int Int
+split beams pos =
+  beams
+    // [ (pos, 0),
+         (left, beams ! left + beams ! pos),
+         (right, beams ! right + beams ! pos)
+       ]
+  where
+    left = pos - 1
+    right = pos + 1
+
+-- parse an input line to a list of splitter indices
+splittersOf :: String -> [Int]
+splittersOf s = map fst $ filter (\(_, c) -> c == '^') $ indexed s
+
+-- pair each element of the list with its index
 indexed :: [a] -> [(Int, a)]
 indexed = zip [0 ..]
-
-applySplitters :: Set Int -> [Int] -> (Set Int, Int)
-applySplitters beams splitters = do
-  let hits = beams `intersection` Set.fromList splitters
-  let left = Set.map pred hits
-  let right = Set.map succ hits
-  let beams' = beams `difference` hits `union` left `union` right
-  (beams', length hits)
-
-applySplittersRec :: Set Int -> [[Int]] -> (Set Int, Int)
-applySplittersRec beams [] = (beams, 0)
-applySplittersRec beams (ss : sss) = (finalBeams, splits + restSplits)
-  where
-    (beams', splits) = applySplitters beams ss
-    (finalBeams, restSplits) = applySplittersRec beams' sss
